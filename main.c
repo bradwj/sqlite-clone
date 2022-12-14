b2b57c16f391192982ed37543c677d8db89e8864
Brad Johnson <2021bradley@gmail.com>
2022-12-14 02:27:35 -0600

    Added unit tests with rspec, fixed string length bugs with insert operation

--- main.c
+++ main.c
@@ -18,8 +18,10 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT,
-    PREPARE_SYNTAX_ERROR
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
@@ -30,8 +32,8 @@ typedef enum { EXECUTE_SUCCESS, EXECUTE_TABLE_FULL } ExecuteResult;
 #define COLUMN_EMAIL_SIZE 255
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 typedef struct {
@@ -127,19 +129,40 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
     }
 }
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
+    statement->type = STATEMENT_INSERT;
+    // example query: insert 1 user user@email.com
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer *input_buffer,
                                 Statement *statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        // example query: insert 1 user user@email.com
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",
-                                   &(statement->row_to_insert.id),
-                                   statement->row_to_insert.username,
-                                   statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
     if (strcmp(input_buffer->buffer, "select") == 0) {
         statement->type = STATEMENT_SELECT;
@@ -168,7 +191,7 @@ ExecuteResult execute_select(Statement *statement, Table *table) {
     return EXECUTE_SUCCESS;
 }
 
-ExecuteResult execute_statement(Statement *statement, Table* table) {
+ExecuteResult execute_statement(Statement *statement, Table *table) {
     switch (statement->type) {
         case (STATEMENT_INSERT):
             execute_insert(statement, table);
@@ -179,8 +202,8 @@ ExecuteResult execute_statement(Statement *statement, Table* table) {
     }
 }
 
-Table* new_table() {
-    Table* table = (Table *)malloc(sizeof(Table));
+Table *new_table() {
+    Table *table = (Table *)malloc(sizeof(Table));
     table->num_rows = 0;
     for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
         table->pages[i] = NULL;
@@ -188,7 +211,7 @@ Table* new_table() {
     return table;
 }
 
-void* free_table(Table* table) {
+void *free_table(Table *table) {
     for (int i = 0; table->pages[i]; i++) {
         free(table->pages[i]);
     }
@@ -196,7 +219,7 @@ void* free_table(Table* table) {
 }
 
 int main(int argc, char *argv[]) {
-    Table* table = new_table();
+    Table *table = new_table();
     InputBuffer *input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
@@ -218,6 +241,12 @@ int main(int argc, char *argv[]) {
         switch (prepare_statement(input_buffer, &statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not parse statement.\n");
                 continue;
